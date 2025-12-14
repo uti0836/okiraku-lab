9a8e9e13f096ce439688d5d12101718761dbcb48
uti0836 <152710641+uti0836@users.noreply.github.com>
2025-12-14 20:49:51 +0900

    Deployed d55707b with MkDocs version: 1.6.1

---
+++ javascripts/config.js
@@ -0,0 +1,11 @@
+﻿document$.subscribe(() => {
+    renderMathInElement(document.body, {
+        delimiters: [
+            { left: "$$", right: "$$", display: true },
+            { left: "$", right: "$", display: false },
+            { left: "\\(", right: "\\)", display: false }, // ?? インライン数式
+            { left: "\\[", right: "\\]", display: true }  // ?? ブロック数式
+        ],
+        throwOnError: false
+    });
+});
